02da40fabd2d3a272f545b6eabe71820f9d9cefb
e42241947-Mutiara <e42241947@student.polije.ac.id>
2025-09-23 15:18:59 +0700

    Belajar PHP

---
+++ variabel.php
@@ -0,0 +1,12 @@
+<?php
+$nim="E42241947";
+$nama="Mutiara Ramadhani";
+$prodi="Bisnis Digital";
+echo "Selamat Praktikum";
+echo "<br>";
+echo $nim;
+echo "<br>";
+echo $nama;
+echo "<br>";
+echo $prodi;
+?>
